fe814a52cd3db6574480b30d160e1a7c7dc4d1b4
Luther Chikumba <lchikumb@msudenver.edu>
2018-09-10 13:37:07 -0600

    I'm gpnna try and modify the file.

--- myProject.java
+++ myProject.java
@@ -1,6 +1,7 @@
 public class myProject{
 	public static void main(String [] args){
 		System.out.println("Hello world! \n" + "My name is Luther.");
+		System.out.println("This is another line that I have added to the code.");
 	}		
 
 }
